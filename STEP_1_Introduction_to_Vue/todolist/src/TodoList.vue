c19b1f385ccbaf757688b0417899e2e53d507de8
wenruo <wenruo@qq.com>
2019-10-19 15:50:41 +0800

    use Vue-cli to restructure the todolist

---
+++ STEP_1_Introduction_to_Vue/todolist/src/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input v-model="inputValue">
+    <button @click="handleClick">submit</button>
+    <ul> 
+      <todo-list v-for="(item, index) in list"
+      :key="index"
+      :content="item"
+      :index="index"
+      @delete="handleDelete"   
+      > 
+      </todo-list>  
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem'
+export default {
+  components: {
+    'todo-list': ToDoItem
+  },
+  data: function() {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleClick: function() {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete: function(index) {
+      this.list.splice(index, 1)
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ STEP_1_Introduction_to_Vue/todolist/src/components/ToDoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="item" @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+ props: ['content', 'index'],
+ methods: {
+  handleClick: function(index) {
+    this.$emit('delete', index)
+  } 
+ }
+};
+</script>
+
+
+<style scoped>
+.item {
+  color: green;
+}
+
+
+</style>
