51d725af70a3fe77829a05635c080aa628105868
wenruo <wenruo@qq.com>
2019-10-29 03:53:08 +0800

    Project init.

---
+++ STEP_3_The_Project/travel/src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+  .home {
+    font-size: 50px;
+  }
+</style>
