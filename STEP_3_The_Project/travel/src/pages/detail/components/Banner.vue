1ceeabf7d3158f4415cf44a8bad0fd34beebb4b1
wenruo <wenruo@qq.com>
2019-11-14 23:26:48 +0800

    banner finish.

---
+++ STEP_3_The_Project/travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1507/72/72d087ebb95580c2.img.jpg_600x330_f4701f0e.jpg">
+      <div class="banner-info">
+        <div class="banner-title">南沙湿地公园</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe647;</span>39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGalleryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1411/93/4bf4b62f71008cab655a079edc90bdb2.water.jpg_r_800x800_46fdfff2.jpg', 'http://img1.qunarzz.com/sight/p0/201405/16/77825dc93469772e88935a4343fe4732.jpg_r_800x800_9aea72ed.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGalleryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        padding: 0 .4rem
+        margin-top: .14rem
+        height: .32rem
+        line-height: .32rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
